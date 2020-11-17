3a06f2123d3c84a0154ff8c9abdc70625b0035e8
Adrien RICHARD <richardadrien0@gmail.com>
2020-11-17 14:16:06 +0100

    [mod] better screen

--- IOT/src/main.cpp
+++ IOT/src/main.cpp
@@ -13,60 +13,58 @@ DHTesp dht;
 
 void setup()
 {
-  Serial.begin(115200); // Start the Serial communication to send messages to the computer
-  delay(10);
+  Serial.begin(115200);
+  lcd.init();
+  lcd.backlight();
+  dht.setup(14, DHTesp::DHT11);
 
-  WiFi.begin(ssid, password); // Connect to the network
-  Serial.print("Connecting to ");
-  Serial.print(ssid);
-  Serial.println(" ...");
+  WiFi.begin(ssid, password);// Connect to the network
+  WiFi.hostname(uuid); 
+  lcd.setCursor(0, 0);
+  lcd.print("Connecting to ");
+  lcd.setCursor(0, 1);
+  lcd.print(ssid);
+  lcd.setCursor(0, 2);
 
-  int i = 0;
   while (WiFi.status() != WL_CONNECTED)
   { // Wait for the Wi-Fi to connect
     delay(1000);
-    Serial.print(++i);
-    Serial.print(' ');
+    lcd.print(".");
   }
-
-  Serial.println('\n');
-  Serial.println("Connection established!");
-  Serial.print("IP address:\t");
-  Serial.println(WiFi.localIP()); // Send the IP address of the ESP8266 to the computer
-
-  lcd.init(); // initialize the lcd
-  // Print a message to the LCD.
-  lcd.backlight();
-  lcd.setCursor(3, 0);
-  lcd.print("Hello, world!");
-  lcd.setCursor(2, 1);
-  lcd.print("Ywrobot Arduino!");
   lcd.setCursor(0, 2);
-  lcd.print("Arduino LCM IIC 2004");
-  lcd.setCursor(2, 3);
-  lcd.print("Power By Ec-yuan!");
 
-  Serial.println("Status\tHumidity (%)\tTemperature (C)\t(F)\tHeatIndex (C)\t(F)");
-  dht.setup(14, DHTesp::DHT11);
+  lcd.print("Connected !");
+  lcd.setCursor(0, 3);
+  lcd.print(WiFi.localIP());
+  delay(2000);
+  lcd.clear();
 }
 
 void loop()
 {
-  delay(dht.getMinimumSamplingPeriod());
+  lcd.clear();
 
   float humidity = dht.getHumidity();
   float temperature = dht.getTemperature();
 
-  Serial.print(dht.getStatusString());
-  Serial.print("\t");
-  Serial.print(humidity, 1);
-  Serial.print("\t\t");
-  Serial.print(temperature, 1);
-  Serial.print("\t\t");
-  Serial.print(dht.toFahrenheit(temperature), 1);
-  Serial.print("\t\t");
-  Serial.print(dht.computeHeatIndex(temperature, humidity, false), 1);
-  Serial.print("\t\t");
-  Serial.println(dht.computeHeatIndex(dht.toFahrenheit(temperature), humidity, true), 1);
-  delay(1000);
+  lcd.setCursor(0, 0);
+  lcd.print("Temp:");
+  lcd.print(temperature, 1);
+
+  lcd.setCursor(0, 1);
+  lcd.print("Humi:");
+  lcd.print(humidity, 1);
+
+  lcd.setCursor(0, 2);
+  lcd.print("C02:");
+  lcd.print(random(5000));
+
+  lcd.setCursor(0, 3);
+  lcd.print("PM2.5:");
+  lcd.print(random(500));
+
+  Serial.println(dht.getStatusString());
+  Serial.println(humidity);
+  Serial.println(temperature);
+  delay(dht.getMinimumSamplingPeriod() + 9000);
 }
