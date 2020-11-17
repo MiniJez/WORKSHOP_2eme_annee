f8d34e60d0306581f39e2c32542bc319e622c747
Pierrick Loiacono <pierrick38800@gmail.com>
2020-11-17 11:34:13 +0100

    Feat: Add return for CO2

--- IA/co2.py
+++ IA/co2.py
@@ -4,4 +4,15 @@ def co2_execution_process(value):
     print("Vérification CO2 : " + str(value))
     print("***")
 
-    #print(Alerts.CO2_Alerte_1.value)
+    if 1000 <= float(value) <= 2000:
+        print(Alerts.CO2_Alerte_1.value)
+        return Alerts.CO2_Alerte_1.value
+    elif 2000 <= float(value) <= 5000:
+        print(Alerts.CO2_Alerte_2.value)
+        return Alerts.CO2_Alerte_2.value
+    elif float(value) <= 5000:
+        print(Alerts.CO2_Alerte_3.value)
+        return Alerts.CO2_Alerte_3.value
+    elif float(value) <= 40000:
+        print(Alerts.CO2_Alerte_4.value)
+        return Alerts.CO2_Alerte_4.value
