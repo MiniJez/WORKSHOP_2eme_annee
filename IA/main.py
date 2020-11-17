218afd164d1e61174de041acbaf234b2a25610c0
Lou BEGE <wolfla@hotmail.fr>
2020-11-17 10:24:43 +0100

    Feat: request API to get the last execution of a sensor

--- IA/main.py
+++ IA/main.py
@@ -1,27 +1,51 @@
+################ Imports ####################
+
 import requests
-import pymongo
-import os
-from dotenv import load_dotenv
+import sched, time, datetime
+from pm import execution_process
+import json
 
-load_dotenv()
-
-MONGODB_URL = os.getenv('MONGODB_URL')
-DB_NAME = os.getenv('DB_NAME')
-TABLE_SENSORS = os.getenv('TABLE_SENSORS')
-TABLE_RAWDATA = os.getenv('TABLE_RAWDATA')
+################ Program ####################
 
 print("Hello World ! I'm the IA !")
 
-# API request
-#response = requests.get("")
-#print(response.status_code)
+#Vérification toutes les minutes
+s = sched.scheduler(time.time, time.sleep)
 
-# Database connection
-#myClient = pymongo.MongoClient(MONGODB_URL)
-#mydb = myClient[DB_NAME]
-#sensorsTable = mydb[TABLE_SENSORS]
-#rawDataTable = mydb[TABLE_RAWDATA]
-#sensorsData = sensorsTable.find()
-#for x in sensorsData:
-#  print(x) 
-#  break
+def ia_process():
+
+    # Récupération des capteurs => /getSensors
+    sensorsDataJson = requests.get("https://eclisson.duckdns.org/ConnectedCity/getSensors").text
+    sensorsData = json.loads(sensorsDataJson)
+    print("Nombre de capteurs : "+ str(len(sensorsData)))
+
+    for sensor in sensorsData:
+        print("Sensor ID : " + sensor["sensorID"][0])
+
+        # Récupération des executions des capteurs => /getSensor/:id => 062336c2-d39b-42cf-a8bb-1d05de74bd7e
+        print("https://eclisson.duckdns.org/ConnectedCity/getSensor/"+sensor["sensorID"][0])
+        rawDataResponse = requests.get("https://eclisson.duckdns.org/ConnectedCity/getSensor/"+sensor["sensorID"][0])
+        print(rawDataResponse.status_code)
+        if rawDataResponse.status_code == 200:
+            rawData = json.loads(rawDataResponse.text)
+            print("Nombre de RawData pour le capteur : "+ str(len(rawData)))
+            rawList = sorted(rawData, key=lambda item: item['time'], reverse=True)
+
+            # Récupération de la dernière exécution du capteur
+            lastExecution = rawList[0]
+            s, ms = divmod(int(lastExecution["time"]), 1000)
+            print("Last execution : ", '%s.%03d' % (time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(s)), ms))
+
+            # # Vérification
+            execution_process(lastExecution["PM25"])
+            print("Vérification Température")
+            print("Vérification Humidité")
+            print("Vérification CO2")
+            break
+    
+    #s.enter(60, 1, ia_process, (sc,))
+
+#s.enter(60, 1, ia_process, (s,))
+#s.run()
+
+ia_process()
---
+++ IA/pm.py
@@ -0,0 +1,2 @@
+def execution_process(value):
+    print("Vérification PM 2.5")
