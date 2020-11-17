07b71d29f534b54632a9ee92269c568a3a04e386
Lou BEGE <wolfla@hotmail.fr>
2020-11-17 11:42:29 +0100

    Feat: Add return for Humidity

--- IA/humidity.py
+++ IA/humidity.py
@@ -1,3 +1,12 @@
+from alerts import Alerts
+
 def humidity_execution_process(value):
     print("Vérification Humidité : " + str(value))
+    if float(value) >= 60:
+        print("Attention : "+Alerts.Humidite_Alerte_1.value)
+        return Alerts.Humidite_Alerte_1.value
+    elif float(value) <= 40:
+        print("Attention : "+Alerts.Humidite_Alerte_2.value)
+        return Alerts.Humidite_Alerte_2.value
+
     print("***")
