d850325c322be473da769bbc489a2da6d82d96cf
Lou BEGE <wolfla@hotmail.fr>
2020-11-17 11:35:38 +0100

    Feat: Add return for PM25

--- IA/pm25.py
+++ IA/pm25.py
@@ -1,2 +1,22 @@
+from alerts import Alerts
+
 def pm25_execution_process(value):
     print("Vérification PM 2.5 : " + str(value))
+
+    if 12.1 <= float(value) <= 35.4:
+        print("Attention : "+Alerts.PM_Alerte_1.value)
+        return Alerts.PM_Alerte_1.value
+    elif 35.5 <= float(value) <= 55.4:
+        print("Attention : "+Alerts.PM_Alerte_2.value)
+        return Alerts.PM_Alerte_2.value
+    elif 55.5 <= float(value) <= 150.4:
+        print("Attention : "+Alerts.PM_Alerte_3.value)
+        return Alerts.PM_Alerte_3.value
+    elif 150.5 <= float(value) <= 250.4:
+        print("Attention : "+Alerts.PM_Alerte_4.value)
+        return Alerts.PM_Alerte_4.value
+    elif 250.5 <= float(value) <= 500.4:
+        print("Attention : "+Alerts.PM_Alerte_5.value)
+        return Alerts.PM_Alerte_5.value
+    
+    print("***")
