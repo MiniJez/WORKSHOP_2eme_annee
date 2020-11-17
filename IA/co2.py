e8f137582db1db8e558b2139b37b3cadddc3cc6f
Pierrick Loiacono <pierrick38800@gmail.com>
2020-11-17 16:14:12 +0100

    Feat: add return in some files

--- IA/co2.py
+++ IA/co2.py
@@ -1,19 +1,20 @@
 from alerts import Alerts
 
 def co2_execution_process(value):
-    print("***")
     print("Vérification CO2 : " + str(value))
 
     if 1000 <= float(value) <= 2000:
-        print(Alerts.CO2_Alerte_1.value)
-        return Alerts.CO2_Alerte_1.value
+        print(Alerts.CO2_Alerte_1)
+        return Alerts.CO2_Alerte_1
     elif 2000 <= float(value) <= 5000:
-        print(Alerts.CO2_Alerte_2.value)
-        return Alerts.CO2_Alerte_2.value
+        print(Alerts.CO2_Alerte_2)
+        return Alerts.CO2_Alerte_2
     elif float(value) <= 5000:
-        print(Alerts.CO2_Alerte_3.value)
-        return Alerts.CO2_Alerte_3.value
+        print(Alerts.CO2_Alerte_3)
+        return Alerts.CO2_Alerte_3
     elif float(value) <= 40000:
-        print(Alerts.CO2_Alerte_4.value)
-        return Alerts.CO2_Alerte_4.value
+        print(Alerts.CO2_Alerte_4)
+        return Alerts.CO2_Alerte_4
         
+    print("***")
+    return ''
--- IA/humidity.py
+++ IA/humidity.py
@@ -4,8 +4,10 @@ def humidity_execution_process(value):
     print("***")
     print("Vérification Humidité : " + str(value))
     if float(value) >= 60:
-        print("Attention : "+Alerts.Humidite_Alerte_1.value)
-        return Alerts.Humidite_Alerte_1.value
+        print("Attention : "+Alerts.Humidite_Alerte_1)
+        return Alerts.Humidite_Alerte_1
     elif float(value) <= 40:
-        print("Attention : "+Alerts.Humidite_Alerte_2.value)
-        return Alerts.Humidite_Alerte_2.value
+        print("Attention : "+Alerts.Humidite_Alerte_2)
+        return Alerts.Humidite_Alerte_2
+
+    return ''
