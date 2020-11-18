7e967de3f622b1945591ddfd1da73e2c17c7adee
Lou BEGE <wolfla@hotmail.fr>
2020-11-18 14:03:54 +0100

    Feat: Init components

---
+++ APP_MOBILE/components/finishedActions.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    StatusBar,
+  } from 'react-native';
+
+class FinishedActions extends Component {
+    render() {
+      return (
+        <Text>Here your finished actions!</Text>
+      );
+    }
+  }
+  
+export default FinishedActions;
---
+++ APP_MOBILE/components/neededActions.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    StatusBar,
+  } from 'react-native';
+
+class NeededActions extends Component {
+    render() {
+      return (
+        <Text>Here your needed actions!</Text>
+      );
+    }
+  }
+  
+export default NeededActions;
