2ad197a8226e036df42d3ad9be03baaf071eb277
Marc-Antoine <marco38000@gmail.com>
2020-11-19 20:27:14 +0100

    Feat : Add initial commit on prod

--- WEB-SITE/src/app/app.component.ts
+++ WEB-SITE/src/app/app.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import * as L from 'leaflet';
+import { Component, OnInit } from '@angular/core';
 import 'leaflet.markercluster';
 import 'leaflet-easybutton';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
