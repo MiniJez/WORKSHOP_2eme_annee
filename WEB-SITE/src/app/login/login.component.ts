1a2c4544b8575af5b6ed506f1da28ff37acd550e
Astraalito <nicolas.asri@epsi.fr>
2020-11-19 14:28:13 +0100

    :construction: update on login feature

--- WEB-SITE/src/app/login/login.component.ts
+++ WEB-SITE/src/app/login/login.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NgxSpinnerService } from "ngx-spinner";
 
 @Component({
   selector: 'app-login',
@@ -13,10 +16,27 @@ export class LoginComponent {
     password: new FormControl(''),
   });
 
-  onSubmit() {
+  constructor(
+    public http: HttpClient,
+    private route: ActivatedRoute,
+    public router: Router,
+    private spinner: NgxSpinnerService,
+    private zone: NgZone,
+    ){}
+
+  async getToken(email : any, password : any) {
+    var body = { "email": email, "password": password }
+    return this.http.post<any>(`https://eclisson.duckdns.org/ConnectedCity/login`, body).toPromise();
+  }
+
+  async onSubmit() {
     console.warn(this.loginForm.value);
-    if((this.loginForm.value.email == "meerky@example.fr")&&(this.loginForm.value.password == "jepue") ) {
-      console.warn("exact");
+    //let myToken = await this.getToken(this.loginForm.value.email, this.loginForm.value.password)
+    //if(myToken.token != null){
+    if((this.loginForm.value.email == "meerky@example.fr")&&(this.loginForm.value.password == "test") ) {
+      //console.warn(myToken);
+      const navigationDetails: string[] = ['/map'];
+      this.router.navigate(navigationDetails)
     }
   }
 
